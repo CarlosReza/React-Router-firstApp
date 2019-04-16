65a8462d36ff82666e85e4683e345bc4e841ed8f
Carlos Reza <carlosrezam@gmail.com>
2019-04-16 13:09:54 -0500

    first commit

---
+++ src/components/Global/Footer.js
@@ -0,0 +1,21 @@
+//Dependencies
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './css/Footer.css';
+
+class Footer extends Component{
+    static propTypes={
+        copyright : PropTypes.string
+    }
+    render(){
+        const {copyright = '&copy; React 2019'} = this.props;
+        return(
+        <div className="Footer">
+        <span dangerouslySetInnerHTML={{ __html:copyright}} />
+        </div>
+        )
+        }
+    
+}
+
+export default Footer;
---
+++ src/components/Page404/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+class Page404 extends Component{
+render(){
+    return(
+        <h1>Welcome to Home</h1>
+    )
+}
+
+}
+
+export default Page404;
